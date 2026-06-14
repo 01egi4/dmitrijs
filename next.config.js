36ee84bbc2e68afb84e5e81cee5e8f269a964f7c
Olegs Celinskis <olegs.c@outlook.com>
2026-06-14 21:52:40 +0300

    domain added

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
-const repo = "dmitrijs";
-
 module.exports = {
   output: "export",
-  basePath: `/${repo}`,
-  assetPrefix: `/${repo}/`,
-  trailingSlash: true,
   images: {
     unoptimized: true
   }
