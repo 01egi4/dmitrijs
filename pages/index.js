dda979d12d5561ae0ab0785552fce9a31cf4360b
Olegs Celinskis <olegs.c@outlook.com>
2026-06-14 14:46:28 +0300

    small fix

--- pages/index.js
+++ pages/index.js
@@ -5,20 +5,27 @@ const PAGE_ID = "7d267919b1194868bef50cc4ee2f0379";
 
 export default function Home({ recordMap }) {
   return (
-    <main>
-      <NotionRenderer
-        recordMap={recordMap}
-        fullPage={true}
-        darkMode={false}
-        disableHeader={true}
-      />
-    </main>
+    <NotionRenderer
+      recordMap={recordMap}
+      fullPage={true}
+      darkMode={false}
+      disableHeader={true}
+      previewImages={true}
+      showCollectionViewDropdown={true}
+      showTableOfContents={false}
+      minTableColumnWidth={120}
+    />
   );
 }
 
 export async function getStaticProps() {
   const notion = new NotionAPI();
-  const recordMap = await notion.getPage(PAGE_ID);
+
+  const recordMap = await notion.getPage(PAGE_ID, {
+    fetchCollections: true,
+    fetchMissingBlocks: true,
+    signFileUrls: true
+  });
 
   return {
     props: {
