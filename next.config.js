9aef92317441fea334bfa9f47b33e86076ee3c95
Olegs Celinskis <olegs.c@outlook.com>
2026-06-14 14:40:31 +0300

    Fix GitHub Pages asset paths

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
+const repo = "dmitrijs";
+
 module.exports = {
   output: "export",
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
+  trailingSlash: true,
   images: {
     unoptimized: true
   }
