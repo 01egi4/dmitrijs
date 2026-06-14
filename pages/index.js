ad7ecf03801bde1930b0ff2222b13b0aeeac9c5b
Olegs Celinskis <olegs.c@outlook.com>
2026-06-14 14:52:59 +0300

    fix

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,34 @@
 import { NotionAPI } from "notion-client";
 import { NotionRenderer } from "react-notion-x";
+import { getPageTableOfContents } from "notion-utils";
 
 const PAGE_ID = "7d267919b1194868bef50cc4ee2f0379";
 
 export default function Home({ recordMap }) {
   return (
-    <NotionRenderer
-      recordMap={recordMap}
-      fullPage={true}
-      darkMode={false}
-      disableHeader={true}
-      previewImages={true}
-      showCollectionViewDropdown={true}
-      showTableOfContents={false}
-      minTableColumnWidth={120}
-    />
+    <main>
+      <NotionRenderer
+        recordMap={recordMap}
+        fullPage={true}
+        darkMode={false}
+        disableHeader={true}
+        showCollectionViewDropdown={true}
+        showTableOfContents={false}
+        minTableOfContentsItems={100}
+      />
+    </main>
   );
 }
 
 export async function getStaticProps() {
-  const notion = new NotionAPI();
+  const notion = new NotionAPI({
+    activeUser: undefined,
+    authToken: undefined
+  });
 
   const recordMap = await notion.getPage(PAGE_ID, {
-    fetchCollections: true,
     fetchMissingBlocks: true,
+    fetchCollections: true,
     signFileUrls: true
   });
 
